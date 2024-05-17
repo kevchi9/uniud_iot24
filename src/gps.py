247ea620bf511a49a64c3ed21f6a57474831dd88
kevchi9 <kevchi9@yahoo.it>
2024-05-17 09:39:31 +0200

    General refactoring, removed unused imports.

--- src/gps.py
+++ src/gps.py
@@ -1,5 +1,8 @@
 import math
-l=46.08122267796358
+
+# Do we need to keep this?
+
+l = 46.08122267796358
 l_long = 111132.92 - 559.82 * math.cos (2*l) + 1.175 * math.cos(4*l) - 0.0023 * math.cos(6*l)
 l_lat = 111412.84 * math.cos(l) - 93.5 * math.cos(3*l) + 0.118 * math.cos(5*l)
 
--- src/json_to_serialtxt.py
+++ src/json_to_serialtxt.py
@@ -1,35 +1,37 @@
-import json
 import pickle
 
-
-# Load the JSON data
-with open('batch0.pickle', 'rb') as logfile:
-    data_batch = pickle.load(logfile)
-
-def filter_json(json_data):
-    electrics = {}
-    electrics_to_keep = ['airflowspeed', 'parkingbrake', 'oil', 'engineRunning',
+# I would use CONSTANTS for this purpose
+ELECTRICS = ['airflowspeed', 'parkingbrake', 'oil', 'engineRunning',
                      'steering', 'abs', 'gear', 'clutch_input', 'throttle_input',
                      'brake_input', 'rpm', 'reverse', 'odometer', 'wheelspeed', 'esc_active',
                      'oil_temperature', 'water_temperature']
+
+# We could do this dynamic to reuse it for other kind of data
+def filter_json(json_data, fields_to_keep):
+    electrics = {}
+    
     for entry, value in json_data['electrics'].items():
-        for save_entry in electrics_to_keep:
-            if entry == save_entry:
-                electrics[save_entry] = value
+        if entry in fields_to_keep:
+            electrics[entry] = value
 
     for tyre, value in json_data['electrics']['wheelThermals'].items():
         electrics[tyre + '_CoreTemperature'] = value['brakeCoreTemperature']
         electrics[tyre + '_SurfaceTemperature'] = value['brakeSurfaceTemperature']
+
     return electrics
 
 def append_entry(json):
     string = ''
-    for key, value in json.items():
+    for _, value in json.items():
         string = string + str(value) + ','
     return string
 
+# Load the JSON data
+with open('../source_data/batch0.pickle', 'rb') as logfile:
+    data_batch = pickle.load(logfile)
+
 # Create a new file txt to write the data (each line every 50ms)
-with open('to_serial.txt', 'w') as f:
-    for key, value in data_batch.items():
-        value = filter_json(value)
+with open('../source_data/to_serial.txt', 'w') as f:
+    for _, value in data_batch.items():
+        value = filter_json(value, ELECTRICS)
         f.write(append_entry(value) + '\n')
